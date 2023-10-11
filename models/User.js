d9188cdbcadefe60e9ae51b1ad89086874421370
tertius Jubber <tjubber@gmail.com>
2023-10-11 20:45:17 +0200

    setting up server

--- models/User.js
+++ models/User.js
@@ -2,9 +2,15 @@ const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-  userName: { type: String, unique: true },
-  email: { type: String, unique: true },
-  password: String
+  userName: {
+     type: String,
+     unique: true },
+  email: {
+     type: String,
+     unique: true },
+  password: {
+    type: String
+  }
 })
 
 
